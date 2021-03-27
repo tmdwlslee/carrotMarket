4ccac104185ac74de18912cd8519494aba234e87
wntjdjus <wntjdjus@naver.com>
2021-03-27 17:49:59 +0900

    OK!

---
+++ src/main/java/mocarim/mocarim/MiracomApplication.java
@@ -0,0 +1,13 @@
+package mocarim.mocarim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiracomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiracomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mocarim/mocarim/MiracomApplicationTests.java
@@ -0,0 +1,13 @@
+package mocarim.mocarim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiracomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
